030ff4fe7d8cc266212a0780a85b2e093ff792fc
Zeeshan6871 <furqanspn@gmail.com>
2024-07-29 12:00:22 +0530

    first commit

---
+++ client/src/components/Router.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  BrowserRouter as Routers,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Register from "./Register";
+import App from "./App";
+import Login from "./Login";
+import Footer from "./Footer";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000";
+
+var Router = () => {
+  return (
+    <Routers>
+      <Switch>
+        <Route path="/" exact component={App}></Route>
+        <Route path="/register" component={Register}></Route>
+        <Route path="/login" component={Login}></Route>
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+      <Footer />
+    </Routers>
+  );
+};
+
+export default Router;
---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const UserRouter = require("./routes/usersRoute");
+const NotesRouter = require("./routes/notesRoute");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const uri = process.env.ATLAS_URI;
+
+console.log(uri);
+mongoose.connect(uri, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection is established.");
+});
+
+app.use("/users", UserRouter);
+app.use("/notes", NotesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+
+app.listen(port, () => {
+  console.log(`Server is running in port: ${port}`);
+});
